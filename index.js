3eea01396761cc522053582bfbc54467ef6af7b8
Angela Pace <angpace13@gmail.com>
2022-09-25 16:35:46 -0400

    commit tree 016f06b91812703d5a89fe64273138b1c995b437

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+//build our fetch
+function submitData(name, email){
+const message =  "Unauthorized Access";
+
+return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json",
+    },
+    body: JSON.stringify({
+        name:`${name}`,
+        email: `${email}`,
+    })
+},
+)
+.then(res => res.json())
+.then(data => document.body.append(data.id))
+.catch((res) => {
+    if (res.ok) {
+        console.log("ok")
+    } else { document.body.append(message)}
+})
+
+
+}
+
+
+submitData("Angela", "angelapace96@aol.com")
+
